41db56fe4c2c3c2af4464bf255d0806ea4ed770a
Akshay Yadav <152361590+Akshay0040@users.noreply.github.com>
2025-01-10 20:08:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+
+console.log("Akshay")
+
+let name = "Akshay"
+let age = 19
+let isLoggedIn = false // data types 
+
+
+
+// number => 2 to power 53
+// bigint , (used on big websites like stockmarket)
+// string => ""
+// boolean => true/false
+//null => standalone value 
+// undefined => 
+// symbole => unique 
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object

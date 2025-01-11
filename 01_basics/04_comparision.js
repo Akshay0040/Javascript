8ca99f337fb69ee6d5bae7dd0573326e5d25d8ea
Akshay Yadav <152361590+Akshay0040@users.noreply.github.com>
2025-01-11 08:48:51 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === or == 
+// == compares values but ignores data types.
+// === compares both values and data types.

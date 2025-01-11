08b65cc1f26903f279cfdf12f3c8411f5b405dcd
Akshay Yadav <152361590+Akshay0040@users.noreply.github.com>
2025-01-11 08:21:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = null 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//notes
+// "33" => 33
+//"33abc" => NaN(not a number)
+// true => 1; false => 0 in boolean
+
+
+let isLoggedIn = "akshay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "akshay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
